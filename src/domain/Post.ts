c89a3453c5fd59a30805d83c314ad5dbcea5aea7
Nish7966 <nishitdantali@gmail.com>
2026-03-26 15:19:42 -0400

    Created domain layer

---
+++ src/domain/Post.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Post", postSchema);
