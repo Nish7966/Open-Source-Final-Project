14032a97516c03d65763d23aad4c28364d2b1851
Nish7966 <nishitdantali@gmail.com>
2026-03-27 11:39:13 -0400

    created controller

---
+++ src/presentation/controllers/postController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import {
+  createPostService,
+  getPostsService,
+  likePostService,
+} from "../../application/postService";
+
+export const createPost = async (req: any, res: Response) => {
+  const post = await createPostService(
+    req.body.title,
+    req.body.content,
+    req.user.id,
+  );
+  res.json(post);
+};
+
+export const getPosts = async (req: Request, res: Response) => {
+  const posts = await getPostsService();
+  res.json(posts);
+};
+
+export const likePost = async (req: any, res: Response) => {
+  const post = await likePostService(req.params.id, req.user.id);
+  res.json(post);
+};
